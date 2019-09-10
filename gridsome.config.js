e019ba7e4c87f748289715482273295de2be35de
ldiebold <luke@ldiebold.com>
2019-09-10 22:15:45 +1000

    restructure for cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,12 +7,34 @@
 module.exports = {
   siteName: 'Vuetiful Blog',
   siteUrl: 'https://blog.vuetiful.life',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
   plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
     {
       use: `gridsome-plugin-netlify-cms`,
       options: {
-        publicPath: `/cms`
+        publicPath: `/admin`
       }
-    } 
+    },
   ]
 }
